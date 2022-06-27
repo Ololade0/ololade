121dce7964631e1cc17ff894ed46f3584c153f3d
Ololade0 <adesuyiololade@gmail.com>
2022-06-27 12:52:39 +0100

    My personal code

---
+++ src/Chapter4Exercises/SalesCommission.java
@@ -0,0 +1,11 @@
+package Chapter4Exercises;
+
+import java.util.Scanner;
+
+public class SalesCommission {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        
+    }
+}
