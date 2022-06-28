b60604dc026928ea553bbf927185a53ddf1d7c4b
Ololade0 <adesuyiololade@gmail.com>
2022-06-28 15:49:40 +0100

    My personal code

---
+++ src/Chapter4Exercises/DanglingElseProblem.java
@@ -0,0 +1,17 @@
+package Chapter4Exercises;
+
+public class DanglingElseProblem {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 10;
+        if(x > 5){
+            if (y > 5){
+                System.out.println("x and y are greater than 5");
+            }
+        }
+
+        else{
+            System.out.println("x is lesser than 5");
+        }
+    }
+}
